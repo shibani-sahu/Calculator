e3be60e0c6a3acc41f7739c2e3994298acd97a96
shibani-sahu <sibanisahu29102001@gmail.com>
2025-04-20 02:07:35 +0530

    Added

---
+++ Calc/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import calculator_view
+
+urlpatterns = [
+    path('' , calculator_view, name='Calculator'),
+
+]
---
+++ Calc/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def calculator_view(request):
+    result=None
+    if request.method == 'POST':
+        num1=int(request.POST.get('num1'))
+        num2=int(request.POST.get('num2'))
+        operation=request.POST.get('operation')
+
+        if operation == 'add':
+            result= num1 + num2
+        elif operation == 'subtract':
+            result= num1 -  num2
+        elif operation == 'multiply':
+            result= num1 *num2
+        elif operation == 'divide':
+            result= num1 / num2 if num2!=0 else "Cannot divide by zero"
+
+    return render(request, 'index.html', {'result':result})
+
+# Create your views here.
