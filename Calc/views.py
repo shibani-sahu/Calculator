8facad303053b8aa2c005b12fe929676c6a07d45
shibani-sahu <sibanisahu29102001@gmail.com>
2025-04-20 22:29:44 +0530

    Some changes in frontend

--- Calc/views.py
+++ Calc/views.py
@@ -1,22 +1,51 @@
 from django.shortcuts import render
+import math
 
+
+layout= [
+    ['√' , 'pi ' , '^', '!',],
+    ['AC', '(' , ')' , '%'],
+    ['7' , '8' , '9', '÷' ],
+    ['4' , '5' , '6' ,'×' ],
+    ['1' , '2' , '3' ,'-' ],
+    ['0' , '.' , ' ' ,'+'],
+    ['=']
+]
+
+def calculate_expression(expression):
+    try:
+        expression= expression.replace('×', '*').replace('÷', '/')
+        expression=expression.replace('pi', str(math.pi)).replace('^', '**')
+        expression= expression.replace('%', '/100')
+
+        if '√' in expression:
+            expression= expression.replace('√' , 'math.sqrt(')+')'
+        
+        if '!' in expression:
+            expression= expression.replace('!', '')
+            expression= str(math.factorial(int(expression)))
+
+        return str(eval(expression))
+
+    except:
+        return "Error"
 
 def calculator_view(request):
-    result=None
+    expression= ""
     if request.method == 'POST':
-        num1=int(request.POST.get('num1'))
-        num2=int(request.POST.get('num2'))
-        operation=request.POST.get('operation')
+        btn = request.POST.get('btn')
+        expression = request.POST.get("expression", "")
 
-        if operation == 'add':
-            result= num1 + num2
-        elif operation == 'subtract':
-            result= num1 -  num2
-        elif operation == 'multiply':
-            result= num1 *num2
-        elif operation == 'divide':
-            result= num1 / num2 if num2!=0 else "Cannot divide by zero"
 
-    return render(request, 'index.html', {'result':result})
+        if btn== 'AC':
+            expression='' 
+        elif btn== ' ':
+            expression= expression[:-1]
+        elif btn == '=':
+            expression= calculate_expression(expression)
+        else:
+            expression += btn      
+
+    return render(request, 'index.html', {"layout": layout, "expression": expression})
 
 # Create your views here.
